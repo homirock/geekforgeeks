a8000ded8d5bf8a4e2be0b3ef9a02c5b06130c11
pgiri <parthsarathigiri@gmail.com>
2023-04-12 02:56:27 +0530

    reverse the string and string array

--- reversestring_array.go
+++ reversestring_array.go
@@ -2,14 +2,29 @@ package main
 
 import "fmt"
 
+func reverseString(v string) string {
+	result := ""
+	for _, value := range v {
+		result = string(value) + result
+	}
+	return result
+}
+
 func main() {
 
 	a := []string{"abc", "bcd", "cde", "def"}
-	fmt.Println(a)
-	l := len(a)
-	for i, j := 0, l-1; i < j; i, j = i+1, j-1 {
-		a[i], a[j] = a[j], a[i]
+	var b []string
+	fmt.Println(len(b))
+	for _, v := range a {
+		rev := reverseString(v)
+		b = append(b, rev)
 	}
-	fmt.Println(a)
+
+	fmt.Println(b)
+	l := len(b)
+	for i, j := 0, l-1; i < j; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	fmt.Println(b)
 
 }
